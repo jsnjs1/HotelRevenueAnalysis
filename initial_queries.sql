3cd9abb068efa9973ef1823669b7ffd59b98b8ba
Jason Jose <102111983+jsnjs1@users.noreply.github.com>
2024-03-30 04:55:40 -0500

    initial_queries.sql

--- initial_queries.sql
+++ initial_queries.sql
@@ -6,6 +6,13 @@ select * from dbo.['2019$']
 union
 select * from dbo.['2020$'])
 
+select
+arrival_date_year,
+hotel,
+round(sum((stays_in_week_nights + stays_in_weekend_nights) * adr),2) as revenue 
+from hotels
+group by arrival_date_year ,hotel
+
 select * from hotels
 left join dbo.market_segment$
 on hotels.market_segment = market_segment$.market_segment
